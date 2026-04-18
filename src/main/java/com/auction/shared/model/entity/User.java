2e1012fb119daaaf854a4b141e6e26287e80ec00
hung2007-coder <25020199@vnu.edu.vn>
2026-04-18 11:44:34 +0700

    Add files via upload

--- src/main/java/com/auction/shared/model/entity/User.java
+++ src/main/java/com/auction/shared/model/entity/User.java
@@ -4,12 +4,17 @@
  * 3. Phương thức getRole() và showMenu() được khai báo abstract để ép buộc các lớp con (vai trò cụ thể) phải tự triển khai logic hiển thị và phân quyền riêng biệt.
  * 4. Chứa logic xác thực đăng nhập chung `login()` có thể được sử dụng cho tất cả các loại người dùng.
  */
+package com.auction.shared.model.entity;
 public abstract class User extends Entity {
     protected String name; // Sử dụng protected hoặc tạo getter/setter để lớp con có thể truy cập
     protected String password;
     protected String role;
 
-    public User() {}
+    public User(String name, String password, String role) {
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
 
     public String getUsername() {
         return this.name;
